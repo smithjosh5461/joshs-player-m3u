964443e9446bc2f7b261ce2e9b0b63ffe62d9c7a
Josh Smith <smithjosh5461@gmail.com>
2023-11-14 16:00:23 -0500

    correct video js libary set up

--- src/App.js
+++ src/App.js
@@ -1,94 +1,106 @@
 import React, { useEffect, useRef, useState } from "react";
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
 
-const Video = (props) => {
-  const videoNode = useRef(null);
-  const [player, setPlayer] = useState(null);
-  useEffect(() => {
-    if (videoNode.current) {
-      const _player = videojs(videoNode.current, props);
-      setPlayer(_player);
-      return () => {
-        if (player !== null) {
-          player.dispose();
-        }
-      };
-    }
-  }, []);
-
-  return (
-    <div data-vjs-player>
-      <video ref={videoNode} className="video-js"></video>
-    </div>
-  );
-};
+// This imports the functional component from the previous sample.
+import VideoJS from './VideoJs'
 
 export default function App() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [currentChannel, setCurrentChannel] = useState("https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8");
-  
-  const loadM3u = () => {
-    fetchData();
-  }
+  const [currentChannel,setCurrentChannel] = useState('https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8');
+  const playerRef = React.useRef(null);
+  const initialized = useRef(false)
+  const [M3uUrl, setM3uUrl] = useState('https://bitbucket.org/!api/2.0/snippets/fluxustv/KpaEKX/60c3e0d18b350c95249a40e06665a1f13085ca48/files/FTV-ENG');
+
+  const handleM3UUrlChange = event => {
+    setM3uUrl(event.target.value);
+  };
+
+  useEffect(() => {
+    if (!initialized.current) {
+      initialized.current = true
+      const data = localStorage.getItem('m3uList');
+      console.log(data)
+      if(data){
+        parseM3u(JSON.parse(data));
+        console.log('result loaded from file')
+      }
+    }
+  },[]);
+
   const changeChannel = (e) => {
     const channel = items[e.currentTarget.dataset.idx]
     console.log(channel.url);
-    //console.log(channel);
+    console.log(channel);
     setCurrentChannel(channel.url);
   }
-  async function fetchData() {
+
+  const downloadM3u = async () =>{
     try {
       console.log('download started')
-      const url = 'https://bitbucket.org/!api/2.0/snippets/fluxustv/KpaEKX/60c3e0d18b350c95249a40e06665a1f13085ca48/files/FTV-ENG';
+      const url = M3uUrl;
       const response = await fetch(url, { timeout: 120000 });
       const data = await response.text();
-      console.log('download complete parsing m3u')
-      // Process your M3U data here
-      const parser = require('iptv-playlist-parser')
-      const result = parser.parse(data)
-      //setCurrentChannel(data.items[0].url);
-      //play.sources.src = currentChannel;
-      console.log('parsing complete')
-      //console.log(result.items)
-      
-      //const channelItems = result.items.map((channel) =>
-      //  <li key={channel.name.toString()}>
-      //    {channel.url}
-      //  </li>
-      //);
-      setItems(result.items);
-      setIsLoading(false);
-      //setCurrentChannels(channelItems);
+      console.log('download complete')
+
+      localStorage.setItem('m3uList', JSON.stringify(data));
+
+      await parseM3u(data)
     } catch (error) {
       console.error('Error fetching data:', error);
       return null;
     }
   }
-  const play = {
-    fill: true,
-    fluid: true,
+  const parseM3u = async (data) => {
+      // Process your M3U data here
+      console.log('parsing m3u: ')
+      const parser = require('iptv-playlist-parser')
+      const result = parser.parse(data)
+      console.log('setting channel to first channel')
+      setCurrentChannel(result.items[1].url);
+
+      setItems(result.items);
+      setIsLoading(false);
+  }
+
+  const videoJsOptions = {
     autoplay: true,
     controls: true,
-    preload: "metadata",
-    sources: [
-      {
-        src: currentChannel,
-        type: "application/x-mpegURL"
-      }
-    ]
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: currentChannel,
+      type: 'application/x-mpegURL'
+    }]
   };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      console.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      console.log('player will dispose');
+    });
+  };
+
   return (
-    <div className="App">
-      <ul>
-        {isLoading && <li>Please Load M3u <button onClick={loadM3u}>Load M3u</button></li>}
-        {!isLoading && items.map(function(d, idx){
+    <>
+    <ul>
+      <button onClick={downloadM3u}>Load M3u</button><input onChange={handleM3UUrlChange} value={M3uUrl}></input>
+      { isLoading && <p>no url loaded</p>}
+      {!isLoading && items.map(function(d, idx){
           return (<li key={d.line} onClick={changeChannel} data-idx={idx}>{d.name}</li>)
         })}
-      </ul>
-      <Video {...play} />
-       
-    </div>
+      
+    
+    </ul>
+    { !isLoading &&
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+    }
+    
+    </>
   );
 }
